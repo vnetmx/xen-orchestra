ea61ca76512f02fc42ef6903382eea42ea58b399
badrAZ <azizbibadr@gmail.com>
2018-07-02 09:29:27 +0200

    better form population code

--- packages/xo-web/src/xo-app/backup-ng/new/new-schedule.js
+++ packages/xo-web/src/xo-app/backup-ng/new/new-schedule.js
@@ -130,7 +130,7 @@ export default [
               />
             </FormGroup>
           )}
-          {state.snapshotMode && (
+          {state.computedSnapshotMode && (
             <FormGroup>
               <label>
                 <strong>{_('snapshotRetention')}</strong>
